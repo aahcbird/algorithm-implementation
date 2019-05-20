ee2c0f59c857f44255831e6b580bc60ffeca228d
aahcbird <eniaslovy2@naver.com>
2019-05-20 20:26:58 +0900

    modified vector

--- src/vector.cpp
+++ src/vector.cpp
@@ -8,13 +8,13 @@ template <typename T>
 class my_vector {
 public:
 	my_vector() : cpct_(10), size_(0) {
-		arr = new T[cpct_]();
+		arr_ = new T[cpct_]();
 	}
 	my_vector(int cpct_) : cpct_(cpct_), size_(0) {
-		arr = new T[cpct_]();
+		arr_ = new T[cpct_]();
 	}
 	~my_vector() {
-		delete []arr;
+		delete []arr_;
 	}
 
 	int size() {
@@ -24,19 +24,19 @@ public:
 	void resize(int tmp_size) {
 		T *tmp = new T[tmp_size]();
 		for (int i = 0; i < size_; ++i) {
-			tmp[i] = arr[i];
+			tmp[i] = arr_[i];
 		}
-		delete []arr;
-		arr = tmp;
+		delete []arr_;
+		arr_ = tmp;
 		cpct_ = tmp_size;
 	}
 
 	void clear() {
-		delete []arr;
+		delete []arr_;
 		
 		cpct_ = 10;
 		size_ = 0;
-		arr = new T[cpct_]();
+		arr_ = new T[cpct_]();
 	}
 
 	void push_back(T val) {
@@ -44,28 +44,28 @@ public:
 			resize(cpct_*2);
 		}
 
-		arr[size_] = val;
+		arr_[size_] = val;
 		++size_;
 	}
 
 	void pop_back() {
-		arr[size_-1] = 0;
+		arr_[size_-1] = 0;
 		--size_;
 	}
 
 	T* begin() {
-		return &arr[0];
+		return &arr_[0];
 	}
 
 	T* end() {
-		return &arr[0] + size_;
+		return &arr_[0] + size_;
 	}
 
 	T operator [](int idx) {
-		return arr[idx];
+		return arr_[idx];
 	}
 
-	T *arr;
+	T *arr_;
 	int cpct_;
 	int size_;
 };
