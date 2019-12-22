f1ef3cc11f08da7b9974440863748e53773aa25c
aahcbird <eniaslovy2@naver.com>
2019-12-22 19:35:10 +0900

    modified fenwick

--- tree-family/fenwick.cpp
+++ tree-family/fenwick.cpp
@@ -1,22 +1,24 @@
 struct Fenwick
 {
-	vector<ll> t;
-    void reset(int n)
-    {
-		t.assign(n+1, 0);
+	vector<int> fw;
+
+	void reset(int n) {
+		fw.assign(n+1, 0);
 	}
-    void update(int p, ll v)
-    {
-        for (; p < (int)t.size(); p += (p&(-p))) t[p] += v;
-    }
-    ll query(int r) //finds [1, r] sum
-    {                     
-        ll sum = 0;
-        for (; r; r -= (r&(-r))) sum += t[r];
-        return sum;
-    }
-    ll query(int l, int r) //finds [l, r] sum
-    {
+
+	void update(int idx, int val) {
+		for (; idx < (int)fw.size(); idx += (idx & -idx)) fw[idx] += val;
+	}
+
+	//finds [1, r] sum
+	int query(int r) { 
+		int sum = 0;
+		for (; r; r -= (r&(-r))) sum += fw[r];
+		return sum;
+	}
+
+	//finds [l, r] sum
+	int query(int l, int r) {
 		if(l == 0) return query(r);
 		return query(r) - query(l-1);
 	}
